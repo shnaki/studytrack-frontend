0ddcc78a9918224bcb8c8cc76dca78b744d7618b
shnaki <tehnca-nome@hotmail.co.jp>
2026-02-15 15:39:34 +0900

    chore: import文関連のモジュールと設定を追加
    1. 未使用のimportの削除のためのESLintプラグイン
    2. importの自動並び替えに関するPrettierプラグイン

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,30 @@ import {defineConfig, globalIgnores} from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from 'eslint-config-prettier/flat'
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-unused-vars': 'off', // 標準のルールをオフにする（競合防止）
+      '@typescript-eslint/no-unused-vars': 'off', // TS環境ならこちらもオフ
+      'unused-imports/no-unused-imports': 'error', // 未使用インポートをエラー（自動修正対象）にする
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_', // _で始まる変数は無視
+          args: 'after-used',
+          argsIgnorePattern: '^_', // _で始まる引数は無視
+        },
+      ],
+    },
+  },
   prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
